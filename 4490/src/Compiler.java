3e744006b8c536a5909fb0fecd183c5786fae7c2
nathanael malloch <nathanaelmalloch@yahoo.com>
2013-01-10 00:09:11 -0700

    lots of changes

--- 4490/src/Compiler.java
+++ 4490/src/Compiler.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -12,6 +11,8 @@ import java.util.StringTokenizer;
  */
 public class Compiler {
     private List<String> keyWords;
+    private List<String> tokenList = new ArrayList<String>();
+    private List<String> symbolCheck;
 
     public enum tokenTypesEnum {
         NUMBER,
@@ -61,8 +62,30 @@ public class Compiler {
         keyWords.add("while");
     }
 
+    protected void setSymbolCheck() {
+        symbolCheck = new ArrayList<String>();
+        symbolCheck.add("<");
+        symbolCheck.add(">");
+        symbolCheck.add(")");
+        symbolCheck.add("(");
+        symbolCheck.add("[");
+        symbolCheck.add("]");
+        symbolCheck.add("{");
+        symbolCheck.add("}");
+        symbolCheck.add(",");
+        symbolCheck.add(".");
+        symbolCheck.add(";");
+        symbolCheck.add(":");
+        symbolCheck.add("=");
+        symbolCheck.add("+");
+        symbolCheck.add("-");
+        symbolCheck.add("/");
+        symbolCheck.add("*");
+    }
+
     public void runCompiler(String fileName) {
         setKeyWords();
+        setSymbolCheck();
         BufferedReader file = null;
         int lineCount = 0;
 
@@ -77,7 +100,8 @@ public class Compiler {
 
         try {
             while ((line = file.readLine()) != null) {
-                if (line.trim().isEmpty()) {
+                // check for empty or pure comment lines
+                if (line.trim().isEmpty() || line.trim().startsWith("//")) {
                     continue;
                 }
 
@@ -86,32 +110,66 @@ public class Compiler {
                     line = line.trim().substring(0, line.indexOf("//"));
                 }
 
-                // after removing comments re-check for an empty string
-                if (line.trim().isEmpty()) {
+                // break up string by spaces
+                String[] tokenizer = line.split("[\\s\t]");
+
+                for (String item : tokenizer) {
+                    if (item.trim().isEmpty()) {
+                        continue;
+                    }
+
+                    int originalListSize = tokenList.size();
+                    for (String s : symbolCheck) {
+                        while (item.contains(s) && item.length() > 1) {
+                            item = breakDownToken(item, s);
+                        }
+                    }
+
+                    if (originalListSize == tokenList.size()) {
+                        tokenList.add(item);
+                    }
+                }
+                lineCount++;
+            }
+
+            for (String token : tokenList) {
+
+                if (token.trim().isEmpty()) {
                     continue;
                 }
 
-                // break up string by spaces
-                StringTokenizer tokenizer = new StringTokenizer(line);
-
-                while (tokenizer.hasMoreTokens()) {
-                    String runner = tokenizer.nextElement().toString();
-                    if (runner.matches("^[-|\\+]?[0-9]+$")) {
-                        System.out.println(runner + ":\t" + tokenTypesEnum.NUMBER);
-                    } else if (runner.length() == 1 && runner.matches("^[a-zA-Z]?$")) {
-                        System.out.println(runner + ":\t" + tokenTypesEnum.CHARACTER);
-                    } else if (runner.substring(0,1).matches("[a-zA-Z]") && runner.length() < 80) {
-                        System.out.println(runner + ":\t" + tokenTypesEnum.IDENTIFIER);
-                    }
-
+                if (keyWords.contains(token.trim())) {
+                    System.out.printf("%-15s %s %n", token, tokenTypesEnum.KEYWORD);
+                } else if (token.matches("^[-|\\+]?[0-9]+$")) {
+                    System.out.printf("%-15s %s %n", token, tokenTypesEnum.NUMBER);
+                } else if (token.length() == 1 && token.matches("^[a-zA-Z]?$")) {
+                    System.out.printf("%-15s %s %n", token, tokenTypesEnum.CHARACTER);
+                } else if (token.substring(0, 1).matches("[a-zA-Z]") && token.length() < 80) {
+                    System.out.printf("%-15s %s %n", token, tokenTypesEnum.IDENTIFIER);
+                } else {
+                    System.out.printf("%-15s %s %n", token, tokenTypesEnum.UNKNOWN);
                 }
-                //TODO: all the work
-
-                lineCount++;
             }
         } catch (IOException e) {
             System.out.println("There was an error reading in the file.  Failed on line " + lineCount);
             System.exit(0);
         }
+    }
+
+    private String breakDownToken(String item, String breakDownItem) {
+        if (item.contains(breakDownItem) && item.trim().length() > 1) {
+            tokenList.add(item.trim().substring(0, item.indexOf(breakDownItem)));
+            tokenList.add(breakDownItem);
+            item = item.trim().substring(item.indexOf(breakDownItem)+1, item.length());
+            for (String s : symbolCheck) {
+                while (item.contains(s) && item.length() > 1) {
+                    item = breakDownToken(item, s);
+                }
+            }
+            tokenList.add(item.substring(item.indexOf(breakDownItem)+1, item.length()));
+
+            return item;
+        }
+        return item;
     }
 }

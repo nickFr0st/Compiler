67642c7de474bc861b96961c4930dfc9c9af6cc6
Nathanael Malloch <nathanaelmalloch@yahoo.com>
2013-08-27 21:30:14 -0600

    fixed bug in field declaration

--- 4490/src/project/Compiler.java
+++ 4490/src/project/Compiler.java
@@ -1162,7 +1162,7 @@ public class Compiler {
             // check format: ["[" "]"] ["=" assignment_expression ] ";"
             if (lexicalAnalyzer.getToken().getType().equals(LexicalAnalyzer.tokenTypesEnum.ARRAY_BEGIN.name())) {
 
-                lexicalAnalyzer.getToken();
+                lexicalAnalyzer.nextToken();
                 if (isUnknownSymbol(lexicalAnalyzer.getToken().getType())) {
                     return false;
                 }
